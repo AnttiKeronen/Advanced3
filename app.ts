0eaf96983b168a7be98916c1bb86a74b40c22868
AnttiKeronen <antti.keronen@student.lut.fi>
2025-11-16 14:05:34 +0200

    json korjaus

--- app.ts
+++ app.ts
@@ -1,35 +1,45 @@
 import express, { Request, Response } from "express";
 import path from "path";
+
 const app = express();
 app.use(express.json());
-app.use(express.static(path.join(__dirname, "public")));
+app.use(express.static(path.join(__dirname, "../public")));
+
+app.get("/", (req: Request, res: Response) => {
+  res.sendFile(path.join(__dirname, "../public/index.html"));
+});
+
 app.get("/hello", (req: Request, res: Response) => {
   res.json({ msg: "Hello world!" });
 });
+
 app.get("/echo/:id", (req: Request, res: Response) => {
   res.json({ id: req.params.id });
 });
+
 app.post("/sum", (req: Request, res: Response) => {
-  const nums: number[] = req.body.numbers;
-  const sum = nums.reduce((a, b) => a + b, 0);
+  const numbers: number[] = req.body.numbers || [];
+  const sum = numbers.reduce((a, b) => a + b, 0);
   res.json({ sum });
 });
-type TUser = {
-  name: string;
-  email: string;
-};
+
+type TUser = { name: string; email: string };
 const users: TUser[] = [];
+
 app.post("/users", (req: Request, res: Response) => {
   const newUser = req.body as TUser;
   users.push(newUser);
-  res.json({ msg: "Added" });
+  res.json({ msg: "User successfully added" });
 });
+
 app.get("/users", (req: Request, res: Response) => {
   res.status(201).json(users);
 });
-const PORT = 3000;
+
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
+
 
 
--- dist/app.js
+++ dist/app.js
@@ -7,6 +7,10 @@ const express_1 = __importDefault(require("express"));
 const path_1 = __importDefault(require("path"));
 const app = (0, express_1.default)();
 app.use(express_1.default.json());
+app.use(express_1.default.static(path_1.default.join(__dirname, "../public")));
+app.get("/", (req, res) => {
+    res.sendFile(path_1.default.join(__dirname, "../public/index.html"));
+});
 app.get("/hello", (req, res) => {
     res.json({ msg: "Hello world!" });
 });
@@ -14,12 +18,11 @@ app.get("/echo/:id", (req, res) => {
     res.json({ id: req.params.id });
 });
 app.post("/sum", (req, res) => {
-    const nums = req.body.numbers;
-    const sum = nums.reduce((a, b) => a + b, 0);
+    const numbers = req.body.numbers || [];
+    const sum = numbers.reduce((a, b) => a + b, 0);
     res.json({ sum });
 });
 const users = [];
-app.use(express_1.default.static(path_1.default.join(__dirname, "../public")));
 app.post("/users", (req, res) => {
     const newUser = req.body;
     users.push(newUser);
@@ -28,6 +31,7 @@ app.post("/users", (req, res) => {
 app.get("/users", (req, res) => {
     res.status(201).json(users);
 });
-app.listen(3000, () => {
-    console.log("Server running on port 3000");
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
 });
